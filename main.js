4bd9501fc4477ea65e9ac28025222d4406a5636f
Taylor Liss <tayloraliss@gmail.com>
2017-03-10 14:20:16 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+//*****Express stuff*********
+var express = require('express');
+var app = express();
+
+//*****Handlebars stuff******
+//Create instance of handlebars let it know default layout is 'main'
+//Default layout is the area all the other contents will be inserted
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+//.handlebars extensions are managed by handlebars
+app.engine('handlebars', handlebars.engine);
+//Lets us ignore .handlebars extensions
+app.set('view engine', 'handlebars');
+
+app.set('port', 3000);
+
+//*****Main Page*************
+app.get('/',function(req,res){
+    res.type('text/plain');
+    //Handlebars will render the contents of home.handlebars inside of main.handlebars' {{{body}}} tag
+    res.render('home');
+});
+
+//*****Error Handling********
+app.use(function(req,res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+//***************************
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
